27bafe684660dd0803d2208b4bac3e561aead6a9
Ryan Berckmans <ryanberckmans@gmail.com>
2020-03-01 23:21:06 -0600

    Add mockObserver, useObservedValue

--- src/Components/observer.tsx
+++ src/Components/observer.tsx
@@ -1,3 +1,5 @@
+import { useEffect, useState } from 'react';
+
 export type SubscriptionOnValueChangeHandler<T> = (t: T) => void;
 export type Unsubscribe = () => void;
 export interface Subscription<T> {
@@ -7,9 +9,27 @@ export interface Subscription<T> {
 export interface Observer<T> {
   subscribe: (s: SubscriptionOnValueChangeHandler<T>) => Subscription<T>,
   isObserver: true, // this property is used to
+  getCurrentValue: () => T,
 };
 export function isObserver<T>(o: T | Observer<T>): o is Observer<T> {
   return typeof o === 'object' && (o as any).isObserver;
+}
+
+export function mockObserver<T>(t: T): Observer<T> {
+  return {
+    subscribe: () => {
+      return {
+        unsubscribe: () => {
+          const x = 5;
+          console.info("mockObserver unsubscribe no-op", x)
+          throw new Error("ran mockObserver unsub");
+        }, // console.info prevents tslint error from empty block
+        initialValue: t,
+      };
+    },
+    isObserver: true,
+    getCurrentValue: () => t,
+  };
 }
 
 // TODO ObserverOwner should be named something like "ObservableValue". The idea here is to hide a piece of state inside an object to avoid putting that state in some parentComponent.state to avoid rerendering that parent. Eg. we hide currently selected currency in this object and use observer pattern to trigger descendant rerender without rerendering entire App / janking the whole page.
@@ -34,6 +54,7 @@ export function makeObserverOwner<T>(initialValue: T): ObserverOwner<T> {
   return {
     getCurrentValue: () => currentValue,
     observer: {
+      getCurrentValue: () => currentValue,
       isObserver: true,
       subscribe,
     },
@@ -42,4 +63,13 @@ export function makeObserverOwner<T>(initialValue: T): ObserverOwner<T> {
       subs.forEach(s => s(newT));
     },
   };
+}
+
+// useObservedValue is a react hook to use an observed value as state.
+export function useObservedValue<T>(o: Observer<T>): T {
+  const [t, setT] = useState(undefined as undefined | T);
+  useEffect(() => {
+    return o.subscribe(setT).unsubscribe;
+  });
+  return t === undefined ? o.getCurrentValue() : t;
 }
