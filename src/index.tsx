769d38615ac4b3fcc18b64761adc8e49bae30cd9
Ryan Berckmans <ryanberckmans@gmail.com>
2020-03-05 13:38:23 -0600

    Disable service worker

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,14 @@
-import './arrayFromPolyfill';
-import * as moment from 'moment';
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import {Routes} from './Routes';
+import './arrayFromPolyfill';
 import './index.css';
-import registerServiceWorker from './registerServiceWorker';
+import { unregisterServiceWorker } from './registerServiceWorker';
+import { Routes } from './Routes';
 
 // window.DATA_URI is set index.html from the environment
 // variable REACT_APP_DATA_URI. This allows the dataURI to
 // vary based on build environment (eg. dev, staging, prod).
 
-registerServiceWorker();
+unregisterServiceWorker(); // I've decided to stop using service workers because right now we frequently publish data updates and I'm always telling users to "refresh the page twice" which is a bad experience for them
 
 ReactDOM.render(<Routes/>, document.getElementById('root') as HTMLElement);
